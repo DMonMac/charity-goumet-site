281b62e5f2fc2857791c849169d21bb1cc2c6c6e
Trev <trevor.c.cruz@gmail.com>
2017-10-10 14:10:47 +0800

    fixed state passing

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,8 +2,13 @@ import React, { Component } from 'react';
 
 export default class Home extends Component {
   render() {
+    let restaurant = this.props.data.map(restaurant => <li key={restaurant.id}>{restaurant.name}</li>)
     return(
-      <p>This is a placeholder for the homepage</p>
+      <div>
+        <p>This is a placeholder for the homepage</p>
+        {restaurant}
+      </div>
+
     )
   }
 }
