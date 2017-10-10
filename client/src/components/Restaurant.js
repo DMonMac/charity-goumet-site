a2a55fa5872e08572aeb2aafdcd28928202ad133
Mikako Iijima <mikakokeshi00124@gmail.com>
2017-10-10 14:40:46 +0800

    initial

--- client/src/components/Restaurant.js
+++ client/src/components/Restaurant.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 
-export default class Restaurant extends Component {
+class Restaurant extends Component {
+
   render() {
-    return(
-      <p>This is the restaurant page</p>
-    )
+    return (
+    <div>
+        <div className="restaurant-detail col-md-8">
+        <div className="restaurant-name">restaurant name</div>
+          <div>description....</div>
+            <iframe className="embed-responsive-item"></iframe>
+        </div>
+         <div className="details">
+          <p>about</p>
+          <div>description....</div>
+        </div>
+    </div>
+    );
   }
 }
+
+export default Restaurant;
