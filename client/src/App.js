922692f55ad600b59342c9833bca5bb51d7ac22a
Trev <trevor.c.cruz@gmail.com>
2017-10-10 17:01:35 +0800

    bootstrapped navbar

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Home from './components/Home.js';
 import Restaurant from './components/Restaurant.js';
 import Booking from './components/Booking.js';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
 class App extends Component {
   constructor(props) {
@@ -32,6 +32,23 @@ class App extends Component {
     return (
       <Router>
         <div>
+          <nav className="navbar navbar-inverse">
+            <div className="container">
+              <div className="navbar-header">
+               <a className="navbar-brand">Grub Tasty</a>
+              </div>
+               <ul className="nav navbar-nav navbar-right">
+                  <li className="dropdown">
+                    <a className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Menu <span className="caret"></span></a>
+                    <ul className="dropdown-menu">
+                      <li><Link to="/">Home</Link></li>
+                      <li><Link to="/Restaurant">Restaurant</Link></li>
+                      <li><Link to="/Booking">Booking</Link></li>
+                    </ul>
+                  </li>
+                </ul>
+            </div>
+          </nav>
           <Route exact path="/" render={ () => <Home data={this.state.restaurant}/>
           }/>
           <Route path="/Restaurant" component={Restaurant} />
