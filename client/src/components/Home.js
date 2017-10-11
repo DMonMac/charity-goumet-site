ec201c8f575cfd5f0541d7f6a93826ce7c1a5c0a
Mikako Iijima <mikakokeshi00124@gmail.com>
2017-10-11 10:56:20 +0800

    none

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,35 +1,15 @@
 import React, { Component } from 'react';
 
 export default class Home extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      text: ''
-    }
-  }
-
-  searchRestaurant(event){
-    console.log(event.target.value)
-    this.setState({
-      text: event.target.value
-    })
-  }
-
   render() {
-    let restaurant = this.props.restaurant_data.map(restaurant => <li key={restaurant.id}>{restaurant.name} ({restaurant.seat_amt} seats)</li>)
+    let restaurant = this.props.data.map(restaurant => <li key={restaurant.id}>{restaurant.name}</li>)
     return(
       <div>
         <h1>This is a placeholder for the homepage</h1>
-        <h2>Restaurant search bar</h2>
-        <input
-          onChange={(event) => this.searchRestaurant(event)}
-          value={this.state.text}
-        />
+        <h2>Reastaurant search bar</h2>
         <h2>Restaurant List</h2>
         {restaurant}
       </div>
-
-
     )
   }
 }
