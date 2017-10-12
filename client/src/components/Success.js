97a746c4230ca8452c6acbaaf44c94f41c0137da
Mikako Iijima <mikakokeshi00124@gmail.com>
2017-10-12 16:59:45 +0800

    success-pic change

--- client/src/components/Success.js
+++ client/src/components/Success.js
@@ -7,7 +7,7 @@ class Success extends Component {
     <div>
         <div className="success-page">
           <h3>Your reservation has been successfully!</h3>
-          <span className="thankyou"><img src="http://kids.wanpug.com/illust/illust1779.png"  /></span>
+          <span className="thankyou"><img src="http://jiburi.com/wp-content/uploads/P6100121_1890.jpg"/></span>
 
           <div className="success">Thank your for your reservation!</div>
         </div>
